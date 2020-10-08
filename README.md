f7fc146ba8f734765fcb3b2a5fa8db862cdcd52d
snakehead007 <snakehead007@pm.me>
2020-10-08 15:13:38 +0200

    update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # bricklink-extended-api
 Includes the bricklink api + scrapers and other useful stuff
+
+
+---
+This package has the API of bricklink built in as 'api'.
+The extended api that is build extra on top of it is called 'plus'.
---
+++ test/app.test.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+let brickApiPLus;
+setup =
+    {TOKEN_VALUE:"A3B97841D0224638852478DE638A678E",
+        TOKEN_SECRET:"E83015A616514C6196FC506BB9AC3B07",
+        CONSUMER_SECRET:"9CA17648F7204DB3A1829F27FE7A0F10",
+        CONSUMER_KEY:"99B44D144DE848E795D4228F4C161700",
+    };
+describe('app.js',()=>{
+    describe('intiliazing with require',()=>{
+       it('should not give any expection when require is set up',()=>{
+           
+       });
+    });
+    describe('adding setup variables',()=>{
+
+   })
+});
