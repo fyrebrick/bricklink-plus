ac4f5798744b1d37184ac617772cec80f2cabfc7
snakehead007 <snakehead007@pm.me>
2020-10-16 09:52:59 +0200

    completed Item documentation

--- src/modules/Item.js
+++ src/modules/Item.js
@@ -83,7 +83,7 @@ const Item ={
      * @returns {Promise<subset_resource>} If successful, this method returns a nested list of subset entries as "data" in the response body. Note that the result is grouped by matching. An inner list indicates one matching group of items.
      *
      */
-    getSubsets:(item_type,item_no,{color_id=null,box=null,instruction=null,break_minifigs=null,break_subsets=null}={})=>{
+    getSubsets:(item_type,item_no,{color_id=undefined,box=undefined,instruction=undefined,break_minifigs=undefined,break_subsets=undefined}={})=>{
         let uri = base_url+"/items/"+item_type+"/"+item_no+"/subsets?";
         if(color_id){
             uri+="color_id="+color_id+"&";
@@ -105,7 +105,62 @@ const Item ={
         });
     },
 
-    getPriceGuide:(item_type,item_no,color_id=null,guide_type=null,new_or_used=null,country_code=null,region=null,currency_code=null,vat=null)=>{
+    /**
+     * @method getPriceGuide
+     * @description This method returns the price statistics of the specified item in BrickLink catalog. <strike>Note that returned price does not include VAT</strike>
+     * @param {string} item_type - The type of the item.
+     * @param {string} item_no - Identification number of the item
+     * @param {number} color_id - The color of the item
+     * @param {string} [guide_type] - Indicates that which statistics to be provided. Acceptable values are:
+     * <ul>
+     *     <li>"sold": Gets the price statistics of "Last 6 Months Sales"</li>
+     *     <li>"stock": Gets the price statistics of "Current Items for Sale" (default)</li>
+     * </ul>
+     * @param {string} [new_or_used] - Indicates the condition of items that are included in the statistics. Acceptable values are:
+     * <ul>
+     *     <li>"N": new item (default)</li>
+     *     <li>"U": used item</li>
+     * </ul>
+     * @param {string} [country_code] - The result includes only items in stores which are located in specified country.
+     * <ul>
+     *     <li>If you don't specify both country_code and region, this method retrieves the price information regardless of the store's location</li>
+     * </ul>
+     * @param {string} [region] - The result includes only items in stores which are located in specified region.
+     * <ul>
+     *     <li>Available values are: asia, africa, north_america, south_america, middle_east, europe, eu, oceania</li>
+     *     <li>If you don't specify both country_code and region, this method retrieves the price information regardless of the store's location</li>
+     * </ul>
+     * @param {string} [currency_code] - This method returns price in the specified currency code
+     * <ul>
+     *     <li>If you don't specify this value, price is retrieved in the base currency of the user profile's</li>
+     * </ul>
+     * @param {string} [vat] - Indicates that price will include VAT for the items of VAT enabled stores. Available values are:
+     * <ul>
+     *     <li>"N": Exclude VAT (default)</li>
+     *     <li>"Y": Include VAT</li>
+     *     <li>"O": Include VAT as Norway settings</li>
+     * </ul>
+     * @example
+     * //Retrieves price statistics(currently for sale) of PART #3001old in new condition
+     * getPriceGuide("part","3001old");
+     * @example
+     * //Retrieves price statistics(currently for sale) of PART #3001old in used condition
+     * getPriceGuide("part","3001old",{new_or_used:"U"});
+     * @example
+     * //Retrieves price statistics(last 6 months sales) of PART #3001old in new condition
+     * getPriceGuide("part","3001old",{guide_type:"sold"});
+     * @example
+     * //Retrieves price statistics(last 6 months sales) of PART #3001old in new condition that are ordered from stores which are located in US.
+     * getPriceGuide("part","3001old",{guide_type:"sold",country_code:"US"});
+     * @example
+     * //Retrieves price statistics(currently for sale) of PART #3001old in new condition that are currently for sale in stores which are located in Asia.
+     * getPriceGuide("part","3001old"{region:"asia"});
+     * @example
+     * //Retrieves price statistics(currently for sale in USD) of PART #3001old in new condition
+     * getPriceGuide("part","3001old",{currency_code:"USD"});
+     * @returns {Promise<price_guide_resource>} If successful, this method returns a price guide resource as "data" in the response body.
+     */
+    getPriceGuide:(item_type,item_no,{color_id=undefined,guide_type=undefined,new_or_used=undefined,country_code=undefined,region=undefined,currency_code=undefined,vat=undefined}={})=>{
         let uri = base_url+"/items/"+item_type+"/"+item_no+"/price?";
         if(color_id){
             uri+="color_id="+color_id+"&";
@@ -132,7 +187,13 @@ const Item ={
             console.trace("Promise call rejected: ", err);
         });
     },
-
+    /**
+     * @method getKnownColors
+     * @description This method returns currently known colors of the item
+     * @param {string} item_type - The type of the item.
+     * @param {string} item_no - Identification number of the item
+     * @returns {Promise<known_color_resource>} If successful, this method returns a list of known color as "data" in the response body.
+     */
     getKnownColors:(item_type,item_no)=>{
         let uri = base_url+"/items/"+item_type+"/"+item_no+"/colors";
         return makeCall(uri, "GET").catch((err) => {
@@ -160,6 +221,45 @@ const Item ={
  * @type {Object}
  * @property {meta} meta - metadata of the request
  * @property {Subset[]} data - data of the request
+ */
+
+/**
+ * @typedef price_guide_resource
+ * @type {Object}
+ * @property {meta} meta - metadata of the request
+ * @property {Price_guide[]} data - data of the request
+ */
+
+/**
+ * @typedef known_color_resource
+ * @type {Object}
+ * @property {number} color_id - Color ID
+ * @property {number} quantity - The quantity of items in that color
+ */
+
+/**
+ * @typedef Price_guide
+ * @type {Object}
+ * @property {Object} item - An object representation of the item
+ * @property {string} item.no - Item's identification number in BL catalog
+ * @property {string} item.type - The type of the item
+ * @property {string} new_or_used - Indicates whether the price guide is for new or used (N: New, U: Used)
+ * @property {string} currency_code - The currency code of the price
+ * @property {number} min_price - The lowest price of the item (in stock / that was sold for last 6 months )
+ * @property {number} max_price - The highest price of the item (in stock / that was sold for last 6 months )
+ * @property {number} avg_price - The average price of the item (in stock / that was sold for last 6 months )
+ * @property {number} qty_avg_price - The average price of the item (in stock / that was sold for last 6 months ) by quantity
+ * @property {number} unit_quantity -  The number of times the item has been sold for last 6 months
+ * <p>The number of inventories that include the item</p>
+ * @property {number} total_quantity - The number of items has been sold for last 6 months
+ * <p>The total number of the items in stock</p>
+ * @property {Array} price_detail - A list of objects that represent the detailed information of the price
+ * @property {number} price_detail.quantity - The number of the items in the inventory
+ * @property {number} price_detail.unit_price - The original price of this item per sale unit
+ * @property {string} [price_detail.shipping_available] - (Current Items for Sale) Indicates whether or not the seller ships to your country(based on the user profile)
+ * @property {string} [price_detail.seller_country_code] - (Last 6 Months Sales) The country code of the seller's location
+ * @property {string} [price_detail.buyer_country_code] - (Last 6 Months Sales) The country code of the buyer's location
+ * @property {string} [price_detail.date_ordered] - (Last 6 Months Sales) The time the order was created
  */
 
 /**
