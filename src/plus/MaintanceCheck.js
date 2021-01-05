728681ae0a0965bb41758f3f8602957faafbac54
snakehead007 <snakehead007@pm.me>
2021-01-05 12:46:31 +0100

    added maintanceCHeck daily

--- src/plus/MaintanceCheck.js
+++ src/plus/MaintanceCheck.js
@@ -31,8 +31,32 @@ const MaintanceCheck = {
         })
         });
     },
+    /**
+     * @method daily.
+     * @description Check if the bricklink website is in the its daily maintance or not.
+     * @returns {Boolean} If true, means that bricklink website is in daily maintance
+     * @example
+     * await bricklinkPlus.plus.maintanceCheck.daily();
+     */
     daily: () => {
-
+        return new Promise ((resolve, reject) => {
+            https.get("https://www.bricklink.com/v2/main.page", (resp) => {
+                let data = '';
+                resp.on('data', (chunk) => {
+                    data += chunk;
+                });
+                resp.on('end', async () => {
+                    let $ = cheerio.load(data);     
+                    console.log(data);
+                    const text = await $("center h1").text().trim();
+                    console.log(text);
+                    resolve(text==="302 Found ");
+                });
+    
+            }).on("error", (err) => {
+                reject(err.message);
+            })
+            });
     }
 }
 
